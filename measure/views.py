6e92ffa03237813745244194c07442206cc33ce6
adiazc50 <30503952+adiazc50@users.noreply.github.com>
2020-08-02 17:52:16 -0500

    algunos querys

--- measure/views.py
+++ measure/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Cliente
+from .models import Juegos
+from .models import Prestamo
+from .serializers import ClienteSerializer
+from .serializers import PrestamoSerializer
+from .serializers import JuegosSerializer
 
-# Create your views here.
+
+class ClienteViewSet(viewsets.ModelViewSet):
+    queryset = Cliente.objects.all().order_by('-created')
+    serializer_class = ClienteSerializer
+
+class PrestamoViewSet(viewsets.ModelViewSet):
+    queryset = Prestamo.objects.all().order_by('-created')
+    serializer_class = PrestamoSerializer
+
+class JuegosViewSet(viewsets.ModelViewSet):
+    queryset = Juegos.objects.all().order_by('-created')
+    serializer_class = JuegosSerializer
