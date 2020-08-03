1d6c40b04c8c24661c3e5a29ba1f4df2ad8cc2fa
adiazc50 <30503952+adiazc50@users.noreply.github.com>
2020-08-02 19:06:25 -0500

    MVC

--- backendPelisapp/models.py
+++ backendPelisapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
+import uuid
 
-
-# Create your models here.
 
 class Cliente(models.Model):
     idCliente = models.IntegerField(primary_key=True)
@@ -11,11 +10,9 @@ class Cliente(models.Model):
     celular = models.CharField(verbose_name='cel',max_Length=20)
     email = models.EmailField(verbose_name='correo',max_Length=20)
 
-    def __str__(self):
-        return 
 
 class Prestamo(models.Model):
-    idPrestamos=models.AutoField(verbose_name='idP',primary_key=True)
+    idPrestamos=models.AutoField(primary_key=True)
     nombreCliente=models.CharField(verbose_name='NombreCl',max_Length=20)
     idCliente=models.IntegerField(verbose_name='idC')
     nombreJuego=models.CharField(verbose_name='NombreJ',max_Length=20)
@@ -27,7 +24,7 @@ class Prestamo(models.Model):
 
 class Juegos(models.Model):
     titulo=models.CharField(verbose_name='tituloJ',max_Length=20)
-    nombreJuego=models.CharField(primary_key=True,max_Length=20)
+    nombreJuego=models.CharField(primary_key=True)
     ano=models.IntegerField(verbose_name='Tipo')
     protagonistas=models.CharField(verbose_name='protagonista',max_Length=50)
     director=models.CharField(verbose_name='dir',max_Length=20)
